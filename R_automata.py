a0431954774676b46510c4c7bd10a309f6096153
mjpyeon <pyeon94@gmail.com>
2018-07-29 10:51:09 +0900

    define PFAs and some member functions to verify them

--- R_automata.py
+++ R_automata.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class R_Automata(object):
 
@@ -12,9 +13,6 @@ class R_Automata(object):
         self.final = final
         self.transitions = transitions
 
-    def generate(self):
-        pass
-
     """
     Forward algorithm in "Representing Distributions over Strings with
                           Automata and Grammars", Page 105
@@ -24,7 +22,7 @@ class R_Automata(object):
         n = int(len(string))
         Q = self.nbS
         F = np.zeros((n+1, Q), dtype=np.float64) #F[n][s] = Pr_A(x,q_s), string x = a_1 a_2 ... a_n
-        
+
         #initialize
         F[0] = self.initial
 
@@ -36,11 +34,52 @@ class R_Automata(object):
         T = F[n]@np.transpose(self.final)
 
         return T
-            
+
 
 # a@b
+
+class PFA(R_Automata):
+    def __init__(self, nbL=0, nbS=0, initial=[], final=[], transitions=[]):
+        super(PFA, self).__init__(nbL, nbS, initial, final, transitions)
+
+    def probability_cond(self):
+        if abs(np.sum(initial), 1.0) > 1e-8:
+            return False, "Wrong initial prob"
+        for q in range(self.nbS):
+            total_prob = .0
+            for _, transition in self.transitions.items():
+                total_prob += transition[q,:].sum()
+            total_prob += self.final[q]
+            if abs(total_prob, 1.0) > 1e-8:
+                return False, "Wrong transition prob at state %d"%(q)
+        return True, ""
+
+    def halting_cond(self):
+        pass
+
+    def generate(self):
+        s = np.random.choice(self.nbS, p=self.initial)
+        at_final = False
+        generated = ""
+        while not at_final:
+            # P(x) = sum_E P(x|E)p(E)
+            # get an alphabet
+            a = np.random.choice(self.nbL + 1,
+                                 p=[pr_a[s].sum() for pr_a in self.transitions.values()] + [self.final[s]])
+            if a == self.nbL:
+                return generated
+            else:
+                generated += a
+
+            # get the next state
+            s = np.random.choice(self.nbS,
+                                 p=self.transitions[a][s])
+
+
+
+
 """
-Input Examples from http://pageperso.lif.univ-mrs.fr/~remi.eyraud/scikit-splearn/ 
+Input Examples from http://pageperso.lif.univ-mrs.fr/~remi.eyraud/scikit-splearn/
 """
 ex_initial = np.array([1, 0], dtype=np.float64)
 ex_final = np.array([0, 1/4], dtype=np.float64)
