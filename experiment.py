75d6ea86005a4205c0291aaca1561877f61ca2ba
Yu-Min Kim <green4469@gmail.com>
2018-10-26 15:26:49 +0900

    Haksu Task

--- experiment.py
+++ experiment.py
@@ -1,6 +1,9 @@
 from common_header import *
 
 import PFA_utils
+import argparse
+import random
+
 
 # Read input file
 
@@ -14,6 +17,44 @@ import PFA_utils
 def str2list(s):
     return [int(k) for k in s.split(',')]
 
+def main(args):
+    algorithm = args.algorithm.lower()
+    k_range = args.k_range
+    n_range = args.n_range
+    nbS_range = args.nbS_range
+    nbL_range = args.nbL_range
+    iters = args.iters
+    result_path = args.result_path
+    for i in range(iters):
+        k = random.randrange(*k_range)
+        n = random.randrange(*n_range)
+        nbS = random.randrange(*nbS_range)
+        nbL = random.randrange(*nbL_range)
+        """
+        TO DO:
+            1. generate string w given n
+            2. generate dpfa A given nbS and nbL
+            3. run the given algorithm
+            4. save the result with type_of_algorithm, k, n, nbS, nbL, nbT, and running time to the result path
+        """
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('experiment for the comparision of DP based algorithm and intersection based algorithm')
+
+    parser.add_argument('--algorithm', type=str, help='type of MPS algorithm: DP and intersection')
+    parser.add_argument('--k_range', type=str2list, help='distance limit k')
+    parser.add_argument('--n_range', type=str2list, help='string length n')
+    parser.add_argument('--nbS_range', type=str2list, help='number of states of PFA')
+    parser.add_argument('--nbL_range', type=str2list, help='set size of alphabets')
+    parser.add_argument('--iters', type=int, help='number of iterations')
+    parser.add_argument('--result_path', type=str, help='name of the result')
+
+    args = parser.parse_args()
+    main(args)
+
+
+"""
 
 DPFA_input_files = os.listdir(sys.argv[1])
 k_min_max = str2list(sys.argv[4])
@@ -44,5 +85,6 @@ for input_file in DPFA_input_files:
             f.write('{},{},{:.4f}\n'.format(n, k, interval))
 
 f.close()
+"""
 
 
