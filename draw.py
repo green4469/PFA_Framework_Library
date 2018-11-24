265b00b8faddb990482ae0a634f2dda0427bcea9
Haksu <limhaksu@naver.com>
2018-11-24 16:17:42 +0900

    Visualizing the emphasis of the MPS

--- draw.py
+++ draw.py
@@ -1,11 +1,61 @@
 from graphviz import Digraph
 from common_header import *
-import sys
+import traceback, sys
 from PyQt5 import uic, QtGui
 from PyQt5.QtWidgets import *
 
+# for multithreading in pyQT
+from PyQt5.QtCore import *
+
 
 form_class = uic.loadUiType("draw.ui")[0]
+# for multithreading in pyQT
+class threadSignals(QObject):
+    '''
+    Defines the signals available from a running worker thread.
+    Supported signals are:
+    finished
+        No data
+    error
+        `tuple` (exctype, value, traceback.format_exc() )
+    result
+        `object` data returned from processing, anything
+    progress
+        `int` indicating % progress
+    '''
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+    progress = pyqtSignal(int)
+
+class multithread(QRunnable):
+    def __init__(self, fn, *args, **kwargs):
+        super(multithread, self).__init__()
+        # Store constructor arguments (re-used for processing)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = threadSignals()
+
+        # Add the callback to our kwargs
+        #self.kwargs['progress_callback'] = self.signals.progress
+        
+    def run(self):
+        '''
+        Initialise the runner function with passed args, kwargs.
+        '''
+        # Retrieve args/kwargs here; and fire processing using them
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+        except:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)  # Return the result of the processing
+        finally:
+            self.signals.finished.emit()  # Done
+
 class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
@@ -16,9 +66,12 @@ class MyWindow(QMainWindow, form_class):
         self.lineEdit_2.setText("0")
         self.lineEdit_2.textChanged.connect(self.hamming_distance)
         self.draw.setStyleSheet("background-color:#ffffff;")
-        self.tableWidget.cellClicked.connect(self.clicked)
+        self.tableWidget.itemSelectionChanged.connect(self.clicked)
         self.tableWidget.setColumnWidth(1,900)
         self.dpfa = None
+
+        # for multithreading in pyQT
+        self.threadpool = QThreadPool()
 
     def enter_pressed(self):
         start_time = time.time()
@@ -47,19 +100,32 @@ class MyWindow(QMainWindow, form_class):
         
         # update drawing
         makePNG(self.dpfa)
-        pixmap = QtGui.QPixmap("Digraph.gv.png") 
+        pixmap = QtGui.QPixmap("DPFA.png") 
         self.draw.setPixmap(pixmap)
+
         self.textBrowser.setText(str(load_time)+' seconds')
-        
+
     def hamming_distance(self):
         self.k = int(self.lineEdit_2.text())
 
-    def clicked(self, row, col):
+    def clicked(self):
+        thread = multithread(self.clicked_thread)
+        self.threadpool.start(thread)
+    
+    def clicked_thread(self):
+        if len(self.tableWidget.selectedRanges()) > 0:
+            self.item_selected(self.tableWidget.currentRow())
+        else:
+            makePNG(self.dpfa)
+            pixmap = QtGui.QPixmap("DPFA.png") 
+            self.draw.setPixmap(pixmap)
+
+    def item_selected(self, row):
         item = self.tableWidget.item(row, 1).text()
         emphasizePNG(self.dpfa, item)
-        pixmap = QtGui.QPixmap("Digraph.gv.png") 
+        pixmap = QtGui.QPixmap("DPFA.png") 
         self.draw.setPixmap(pixmap)
-
+        
 def makePNG(RA):
     dot = Digraph(comment='PFA', format='png')
     for i in range(RA.nbS):
@@ -75,7 +141,7 @@ def makePNG(RA):
     dot.attr('node',shape='none')
     dot.node('')
     dot.edge('','0')
-    dot.render()
+    dot.render('DPFA')
 
 def emphasizePNG(RA, string):
     current_node = 0
@@ -105,11 +171,11 @@ def emphasizePNG(RA, string):
                         dot.edge(str(i), str(j), '{}, {}'.format(alphabet,probability),style='bold')
                     else:   
                         probability = round(RA.transitions[alphabet][i,j],2)
-                        dot.edge(str(i), str(j), '{}, {}'.format(alphabet,probability),style='dotted')
+                        dot.edge(str(i), str(j), '{}, {}'.format(alphabet,probability),style='dashed')
     dot.attr('node',shape='none')
     dot.node('')
     dot.edge('','0',style='bold')
-    dot.render() 
+    dot.render('DPFA') 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
